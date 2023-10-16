4b4edde50af92a69beff9e0f9e28730b901c1f53
mmisaacs <misaacs@cpp.edu>
2023-10-15 23:22:06 -0700

    bug fix cipher, now outputs cipher text

--- src/CipherText.java
+++ src/CipherText.java
@@ -4,8 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CipherText implements ActionListener {
+    //declare text field to be accessible outside the method
     public String text;
+
+    //declare text fields to be accessible outside the method
     public JTextField input, output;
+
+    StringBuilder str = new StringBuilder();
     CipherText(){
         //declare frame and exit when closed
         JFrame jfrm = new JFrame ("Simple Cipher Coder");
@@ -20,7 +25,7 @@ public class CipherText implements ActionListener {
         //create text fields
         input = new JTextField(30);
         output = new JTextField(30);
-        output.setEditable(false);
+        //output.setEditable(false);
 
         //create buttons
         JButton enc = new JButton("Encode");
@@ -46,25 +51,49 @@ public class CipherText implements ActionListener {
         jfrm.getContentPane().add(dec);
         jfrm.getContentPane().add(reset);
 
+        //set frame to be visible
         jfrm.setVisible(true);
     }
 
-    public String cipher (String input){
+    //creating method to cipher the text in plain text field
+    public String cipher (String phrase){
+        str.setLength(0); //clears string builder
+        //shift each part of text one forward (ex. a -> b)
+        for (int i = 0; i < phrase.length(); ++i){
+            str.append((char)(phrase.charAt(i) + 1));
+        }
 
-        return "";
+        return str.toString();
+    }
+
+    //when text is entered into "Cipher Text" it will shift letters back one
+    public String decipher (String phrase){
+        str.setLength(0); //clears string builder
+        for (int i = 0; i < phrase.length(); ++i){
+            str.append((char)(phrase.charAt(i) - 1));
+        }
+
+        return str.toString();
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        /*if(ae.getActionCommand().equals("Encode"))
-            //cipher(text);
-        else */
+        //set cipher text to ciphered plain text
+        if(ae.getActionCommand().equals("Encode"))
+            output.setText(cipher(input.getText()));
+
+        //when text entered into cipher text, it will print regular text in "Plain Text"
+        if(ae.getActionCommand().equals("Decode"))
+            input.setText(decipher(output.getText()));
+
+        //text clears when pressed
         if(ae.getActionCommand().equals("Reset")) {
             input.setText("");
             output.setText("");
         }
     }
 
+    //run the program
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
